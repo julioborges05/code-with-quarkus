bc0dc844db68d5e2b388e4dd48598edff32b42ad
Júlio Borges <julioborges05@hotmail.com>
2023-06-19 22:37:22 -0300

    Adds the database connection

--- src/main/java/com/vollmed/controller/DoctorController.java
+++ src/main/java/com/vollmed/controller/DoctorController.java
@@ -1,6 +1,8 @@
 package com.vollmed.controller;
 
 import com.vollmed.domain.doctor.DoctorRegisterData;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -8,8 +10,8 @@ import org.springframework.web.bind.annotation.*;
 public class DoctorController {
 
     @PostMapping
-    public DoctorRegisterData saveNewDoctor(@RequestBody DoctorRegisterData doctorRegisterData) {
-        return doctorRegisterData;
+    public ResponseEntity<DoctorRegisterData> saveNewDoctor(@RequestBody DoctorRegisterData doctorRegisterData) {
+        return new ResponseEntity<>(doctorRegisterData, HttpStatus.OK);
     }
 
 }
